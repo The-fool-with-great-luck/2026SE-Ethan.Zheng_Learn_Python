0bb40b836676d09bf5fc31394f0fac3f06b3b940
Ethan Z <126206506+The-fool-with-great-luck@users.noreply.github.com>
2025-02-06 23:35:05 +0000

    commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+#Asking user for language.
+print("what's your name? ")
+name = input().strip().title()
+
+#remove whitespace from str
+#name = name.strip()
+
+#Capitalize the name
+#name = name.title()
+
+#split uers name into first name and last name
+name.split(" ")
+
+#Say hello to user
+print(f"hello, {name}")

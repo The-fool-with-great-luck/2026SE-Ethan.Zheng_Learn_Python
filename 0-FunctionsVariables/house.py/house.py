95b6f3e0dcfb224bc17a56f6259a0254292f5e8a
Ethan Z <126206506+The-fool-with-great-luck@users.noreply.github.com>
2025-02-20 23:04:35 +0000

    Finished Conditionals vid

--- 0-FunctionsVariables/house.py/house.py
+++ 0-FunctionsVariables/house.py/house.py
@@ -1,8 +1,18 @@
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffndor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Get out!!!")
+name = name.title()
+
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+    # print("Gryffndor")
+# elif name == "Draco":
+    # print("Slytherin")
+# else:
+    # print("Get out!!!")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")

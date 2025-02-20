7f8564091a1ba27b7a91460c538ea0efebca63d0
Ethan Z <126206506+The-fool-with-great-luck@users.noreply.github.com>
2025-02-20 22:48:22 +0000

    All files

---
+++ 0-FunctionsVariables/house.py/house.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffndor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Get out!!!")
---
+++ rps.py
@@ -0,0 +1,9 @@
+name = input("Choose your character! ")
+
+def choice():
+choice = input("1 = Rock, 2 = Paper or 3 = Scissors? ")
+
+def random_choice():
+    return random.radiant(1,3)
+
+choice()
